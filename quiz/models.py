efd2459c6d9b30dfcf91fd5b2e23221c5b976f22
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-29 15:14:55 +0500

    models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content[:50]
+
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return self.content[:50]
+
+
+class Take(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username + ' - ' + self.quiz.name
+
+
+class Response(models.Model):
+    take = models.ForeignKey(Take, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.question.content[:50] + ' - ' + self.option.content[:50]
